0a7130b9652785faf814ab68f10b942f6dccbb35
rabia-syed-103 <bscs24103@itu.edu.pk>
2026-04-27 09:37:12 +0500

    Seed.js done

--- seed.js
+++ seed.js
@@ -17,14 +17,163 @@ require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const { connect } = require('./db/connection');
 
+
 (async () => {
   const db = await connect();
 
   // OPTIONAL: clear existing data so re-seeding is idempotent
-  // await db.collection('users').deleteMany({});
-  // await db.collection('projects').deleteMany({});
-  // await db.collection('tasks').deleteMany({});
-  // await db.collection('notes').deleteMany({});
+  await db.collection('users').deleteMany({});
+  await db.collection('projects').deleteMany({});
+  await db.collection('tasks').deleteMany({});
+  await db.collection('notes').deleteMany({});
+  // Users
+  const u1 = await db.collection('users').insertOne({
+    email: 'user1@gmail.com',
+    name: 'User One',
+    passwordHash: await bcrypt.hash('password123', 10)
+  });
+  const u2 = await db.collection('users').insertOne({
+    email: 'user2@gmail.com',
+    name: 'User Two',
+    passwordHash: await bcrypt.hash('password1233',10)
+  });
+  const userId1 = u1.insertedId;
+  const userId2 = u2.insertedId;
+
+  //Projects
+  const p1 = await db.collection('projects').insertOne({
+    ownerId: userId1,
+    name: 'Project One',
+    archived: false,
+    description: 'Description for project one',
+    createdAt: new Date()
+  });
+  const p2 = await db.collection('projects').insertOne({
+    ownerId: userId1,
+    name: 'Project Two',
+    archived: false,
+    description: 'Description for project Two',
+    createdAt: new Date()
+  });
+  const p3 = await db.collection('projects').insertOne({
+    ownerId: userId2,
+    name: 'Project Three',
+    archived: false,
+    description: 'Description for project three',
+    createdAt: new Date()
+  });
+  const p4 = await db.collection('projects').insertOne({
+    ownerId: userId2,
+    name: 'Project Four',
+    archived: false,
+    description: 'Description for project four',
+    createdAt: new Date()
+  });
+
+  // Tasks
+  t1 = await db.collection('tasks').insertOne({
+    ownerId: userId1,
+    projectId: p1.insertedId,
+    title: 'Task One',
+    status: 'todo',
+    priority: 3,  
+    tags: ['writing', 'urgent'],
+    subtasks: [
+      { title: 'Outline sections', done: true },
+      { title: 'Draft', done: false }
+    ],
+    createdAt: new Date()  });
+
+  t2 = await db.collection('tasks').insertOne({
+    ownerId: userId1,
+    projectId: p1.insertedId,
+    title: 'Task Two',
+    status: 'todo',
+    priority: 3,  
+    tags: ['writing', 'urgent'],
+    subtasks: [
+      { title: 'Outline sections', done: true },
+      { title: 'Draft', done: false }
+    ],
+    createdAt: new Date()  });
+  t3 = await db.collection('tasks').insertOne({
+    ownerId: userId1,
+    projectId: p2.insertedId,
+    title: 'Task Three',
+    status: 'todo',
+    priority: 3,
+    tags: ['writing', 'urgent'],
+    subtasks: [
+      { title: 'Outline sections', done: true },
+      { title: 'Draft', done: false }
+    ],
+    createdAt: new Date()  })
+  t4 = await db.collection('tasks').insertOne({
+    ownerId: userId2,
+    projectId: p3.insertedId,
+    title: 'Task Four',
+    status: 'todo',
+    priority: 3,
+    tags: ['writing', 'urgent'],
+    subtasks: [
+      { title: 'Outline sections', done: true },
+      { title: 'Draft', done: false }
+    ],
+    createdAt: new Date()  })
+  t5 = await db.collection('tasks').insertOne({
+    ownerId: userId2,
+    projectId: p4.insertedId,
+    title: 'Task Five',
+    status: 'todo',    
+    priority: 3,
+    tags: ['writing', 'urgent'],
+    subtasks: [
+      { title: 'Outline sections', done: true },
+      { title: 'Draft', done: false }
+    ],
+    createdAt: new Date()
+  });
+
+  // Notes
+
+  n1 = await db.collection('notes').insertOne({
+    ownerId: userId1,
+    projectId: p1.insertedId,
+    title: 'Note One',
+    body: 'Note for project one',
+    tags: [],
+    createdAt: new Date()
+  });
+  n2 = await db.collection('notes').insertOne({
+    ownerId: userId1,
+    title: 'Note One',
+    tags: [],
+    body: 'Standalone note for user one',
+    createdAt: new Date()
+  });
+  n3 = await db.collection('notes').insertOne({
+    ownerId: userId2,
+    title: 'Note One',
+    projectId: p2.insertedId,
+    tags:[],
+    body: 'Note for project two',
+    createdAt: new Date()
+  });
+  n4 = await db.collection('notes').insertOne({
+    ownerId: userId2,
+    title: 'Note four',
+    tags: [],
+    body: 'Standalone note for user two',
+    createdAt: new Date()
+  });
+  n5 = await db.collection('notes').insertOne({
+    ownerId: userId2,
+    title: 'Note five',
+    tags: [],
+    projectId: p3.insertedId,
+    body: 'Note for project three',
+    createdAt: new Date()
+  });
 
   // =============================================================================
   //  TODO: Insert your seed data below.
@@ -53,7 +202,7 @@ const { connect } = require('./db/connection');
   //      createdAt: new Date()
   //    }
   // =============================================================================
-
-  console.log('TODO: implement seed.js');
+  
+  console.log('implemented seed.js');
   process.exit(0);
 })();
